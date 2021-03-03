879178d8ac7ebef56faaef1ec6427b54680b3837
arunkfedex <arun05111990@gmail.com>
2021-03-03 22:09:40 +0530

    Update MainActivity.kt

--- app/src/main/java/com/arun/androidtutsforu/demonavigation/MainActivity.kt
+++ app/src/main/java/com/arun/androidtutsforu/demonavigation/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding :ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        //setContentView(R.layout.activity_main)
        drawerLayout = binding.draweLayout
         val navController = this.findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
